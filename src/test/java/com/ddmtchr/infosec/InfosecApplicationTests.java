df82e0861b933e3fe5adf356099ff94324415587
Mikhail Romanenko <rmr04489@gmail.com>
2025-09-16 22:57:52 +0300

    init commit

---
+++ src/test/java/com/ddmtchr/infosec/InfosecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddmtchr.infosec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfosecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
